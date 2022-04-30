2652cd8c5930d0d0a17e358e78b6c4c1d6eb8896
Carolina Nonato <csnonato@gmail.com>
2022-04-30 09:38:18 -0400

    learn more button color on click

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import heroImage from '../assets/images/image-hero-desktop.png'
 import Clients from './Clients'
 
 function Hero() {
+    const [buttonColor, setButtonColor] = useState("button1");
+
+
     return (
 
-        <div className="hero-container">
+        <div div className="hero-container" >
             <div className="title">
                 <h1 className="hero-title">Make remote work</h1>
                 <p className="hero-paragraph">Get your team in sync, no matter your location. Streaming processes, create team rituals, and watch productivity soar.</p>
 
-                <button className="hero-button">Learn more</button>
+                <button
+                    className={buttonColor}
+                    onClick={() => setButtonColor((buttonColor) => (buttonColor === "button1" ? "button2" : "button1"))}
+                >
+                    Learn More
+                </button>
                 <Clients />
             </div>
             <div className='hero-image-section'>
@@ -18,7 +26,7 @@ function Hero() {
             </div>
 
 
-        </div>
+        </div >
 
 
     )
